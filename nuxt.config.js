6ce0fe81dfc9a1be5f7f2a1440b07492b548b378
Tom Haepers <tom.haepers@outlook.be>
2023-03-28 22:03:39 +0200

    Typescript fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    // '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
